9dcf494f0f6410a70547ca51ded48c7fad3ea776
evkondr <kondratiev.evg@gmail.com>
2022-05-06 14:54:40 +0300

    feat: add user, users, me -queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -2,7 +2,16 @@ module.exports = {
     notes: async (parent, args, { models }) => {
         return await models.Note.find();
     },
-    note: async (parent, args, { models }) => {
-        return await models.Note.findById(args.id);
+    note: async (parent, { id }, { models }) => {
+        return await models.Note.findById(id);
+    },
+    user: async (parent, { username }, { models }) => {
+        return await models.User.fimdOne({username})
+    },
+    users: async (parent, args, { models }) => {
+        return await models.User.find({});
+    },
+    me: async (parent, args, { models, user }) => {
+        return models.User.findById(user.id)
     }
 }
