7504237817d98719a733ce262d5ff8052ede1d8c
evkondr <kondratiev.evg@gmail.com>
2022-05-18 23:41:00 +0300

    feat: add resolvers note and users for nested queries

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,8 +1,12 @@
 const Mutation = require('./mutation');
 const Query = require('./query');
+const Note = require('./note');
+const User = require('./user');
 const { GraphQLDateTime } = require('graphql-iso-date');
 module.exports = {
     Query,
     Mutation,
+    Note,
+    User,
     DateTime: GraphQLDateTime
 }
